06075131af4d0fd8890e8fdc52777f5a0cb1a029
SONU KANWAR <138689626+sonu6367@users.noreply.github.com>
2024-04-07 19:53:13 +0530

    Update __main__.py

--- Adarsh/__main__.py
+++ Adarsh/__main__.py
@@ -24,7 +24,7 @@ logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
 ppath = "Adarsh/bot/plugins/*.py"
 files = glob.glob(ppath)
-StreamBot.start()
+await StreamBot.start()
 loop = asyncio.get_event_loop()
 
 
